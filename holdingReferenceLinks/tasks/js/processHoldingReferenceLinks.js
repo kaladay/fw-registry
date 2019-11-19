8cd78ea476486caad65b30c2d9c94d5d919c43a2
William Welling <wwelling@library.tamu.edu>
2019-11-19 08:56:41 -0600

    type should be object for reference link batch

--- holdingReferenceLinks/tasks/js/processHoldingReferenceLinks.js
+++ holdingReferenceLinks/tasks/js/processHoldingReferenceLinks.js
@@ -1,6 +1,12 @@
 var holdingTypes = {
-  AMDB: '{{mod-external-reference-resolver}}/referenceLinkTypes/67c65ccb-02b1-4f15-8278-eb5b029cdcd5',
-  MSDB: '{{mod-external-reference-resolver}}/referenceLinkTypes/e7fbdcf5-8fb0-417e-b477-6ee9d6832f12'
+  AMDB: {
+    id: "67c65ccb-02b1-4f15-8278-eb5b029cdcd5",
+    name: "HOLDING_AMDB"
+  },
+  MSDB: {
+    id: "e7fbdcf5-8fb0-417e-b477-6ee9d6832f12",
+    name: "HOLDING_MSDB"
+  }
 };
 
 var holdingUUID = UUID.randomUUID().toString();
