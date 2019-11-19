28b7744e88902a88d0baf14c28b67fcf3f66b7cd
William Welling <wwelling@library.tamu.edu>
2019-11-19 08:55:28 -0600

    type should be object for reference link batch

--- itemReferenceLinks/tasks/js/processItemReferenceLinks.js
+++ itemReferenceLinks/tasks/js/processItemReferenceLinks.js
@@ -1,6 +1,12 @@
 var itemTypes = {
-  AMDB: '{{mod-external-reference-resolver}}/referenceLinkTypes/53e72510-dc82-4caa-a272-1522cca70bc2',
-  MSDB: '{{mod-external-reference-resolver}}/referenceLinkTypes/0014559d-39f6-45c7-9406-03643459aaf0'
+  AMDB: {
+    id: "53e72510-dc82-4caa-a272-1522cca70bc2",
+    name: "ITEM_AMDB"
+  },
+  MSDB: {
+    id: "0014559d-39f6-45c7-9406-03643459aaf0",
+    name: "ITEM_MSDB"
+  }
 };
 
 var itemUUID = UUID.randomUUID().toString();
