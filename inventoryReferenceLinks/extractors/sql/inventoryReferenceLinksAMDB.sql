b85cf19303333d5db80d9ae2fa95130521aca774
William Welling <wwelling@library.tamu.edu>
2019-11-23 13:06:29 -0600

    sql collect for bib, holdings per bib and items per holding

--- inventoryReferenceLinks/extractors/sql/inventoryReferenceLinksAMDB.sql
+++ inventoryReferenceLinks/extractors/sql/inventoryReferenceLinksAMDB.sql
@@ -1,4 +1,7 @@
-SELECT t.bib_id,
-  'AMDB' AS schema
-FROM AMDB.bib_master t
+SELECT bh.bib_id,
+  'AMDB' AS schema,
+  CAST(COLLECT('{"HOLDING_ID":"' || bh.mfhd_id || '","ITEM_ID":"' || hi.item_id || '"}') AS sys.odcivarchar2list) AS holding_items
+FROM AMDB.bib_mfhd bh
+  INNER JOIN AMDB.mfhd_item hi ON bh.mfhd_id = hi.mfhd_id
+GROUP BY bh.bib_id, hi.mfhd_id
 ;
--- inventoryReferenceLinks/extractors/sql/inventoryReferenceLinksMSDB.sql
+++ inventoryReferenceLinks/extractors/sql/inventoryReferenceLinksMSDB.sql
@@ -1,4 +1,7 @@
-SELECT t.bib_id,
-  'MSDB' AS schema
-FROM MSDB.bib_master t
+SELECT bh.bib_id,
+  'MSDB' AS schema,
+  CAST(COLLECT('{"HOLDING_ID":"' || bh.mfhd_id || '","ITEM_ID":"' || hi.item_id || '"}') AS sys.odcivarchar2list) AS holding_items
+FROM MSDB.bib_mfhd bh
+  INNER JOIN MSDB.mfhd_item hi ON bh.mfhd_id = hi.mfhd_id
+GROUP BY bh.bib_id, hi.mfhd_id
 ;
