6ae5c6c84f7e468721885c07dc72f637f11622a9
William Welling <wwelling@library.tamu.edu>
2019-12-05 14:24:03 -0600

    source and instance schema match for enhance

--- bibs/extractors/sql/bibsMSDB.sql
+++ bibs/extractors/sql/bibsMSDB.sql
@@ -5,7 +5,8 @@ WITH
   )
 SELECT /*+ FIRST_ROWS(1000) */ t.bib_id,
   t.suppress_in_opac,
-  'BIB_MSDB_SOURCE' AS schema,
+  'BIB_MSDB_INSTANCE' AS instance_schema,
+  'BIB_MSDB_SOURCE' AS source_schema,
   rtrim(MSDB.getBibBlob(t.bib_id)) AS marc_record,
   (SELECT * FROM total_bibs) AS bib_total
 FROM MSDB.bib_master t
