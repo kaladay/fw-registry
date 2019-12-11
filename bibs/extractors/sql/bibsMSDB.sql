e387f582b4e071f43f3512d792b07c54b8cca9e4
William Welling <wwelling@library.tamu.edu>
2019-12-11 15:52:03 -0600

    remove rtrim from getBibBlob

--- bibs/extractors/sql/bibsMSDB.sql
+++ bibs/extractors/sql/bibsMSDB.sql
@@ -7,7 +7,7 @@ SELECT /*+ FIRST_ROWS(1000) */ t.bib_id,
   t.suppress_in_opac,
   'BIB_MSDB_INSTANCE' AS instance_schema,
   'BIB_MSDB_SOURCE' AS source_schema,
-  rtrim(MSDB.getBibBlob(t.bib_id)) AS marc_record,
+  MSDB.getBibBlob(t.bib_id) AS marc_record,
   (SELECT * FROM total_bibs) AS bib_total
 FROM MSDB.bib_master t
 ORDER BY t.bib_id
