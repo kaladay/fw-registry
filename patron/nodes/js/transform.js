0d6f0f50176ae0582b81057bab0e1885e8f54a26
William Welling <wwelling@library.tamu.edu>
2021-02-04 09:19:01 -0600

    cleanup and normalize postal codes

--- patron/nodes/js/transform.js
+++ patron/nodes/js/transform.js
@@ -1,3 +1,16 @@
+function normalizePostalCode(postalCode) {
+  postalCode = postalCode.trim();
+  if (postalCode) {
+    if (postalCode.endsWith('-')) {
+      postalCode = postalCode.substring(0, postalCode.length - 1);
+    }
+    if (postalCode.length === 9 && !postalCode.includes('-')) {
+      postalCode = postalCode.substring(0, 5) + '-' + postalCode.substring(5);
+    }
+  }
+  return postalCode;
+}
+
 function tranform(patrons) {
   var users = [];
   for (var i = 0; i < patrons.length; i++) {
@@ -5,62 +18,62 @@ function tranform(patrons) {
     var userImport = {
       departments: []
     };
-  
+
     var personal = {
       addresses: []
     };
-  
+
     var patron = patrons[i];
-  
+
     userImport.username = patron.USERNAME;
-  
+
     userImport.externalSystemId = patron.EXTERNALSYSTEMID;
     userImport.barcode = patron.BARCODE;
     userImport.active = patron.ACTIVE;
     userImport.patronGroup = patron.PATRONGROUP;
-  
+
     personal.lastName = patron.PERSONAL_LASTNAME;
     personal.firstName = patron.PERSONAL_FIRSTNAME;
     personal.middleName = patron.PERSONAL_MIDDLENAME;
     personal.email = patron.PERSONAL_EMAIL;
     /* TODO: normalize/format phone number */
     personal.phone = patron.PERSONAL_PHONE;
-  
+
     if (patron.ADDRESSES_PERMANENT_ADDRESSLINE1) {
       var permanentAddress = {};
-  
-      permanentAddress.AddressTypeId  = patron.ADDRESSES_PERMANENT_ADDRESSTYPEID;
-      permanentAddress.CountryId  = patron.ADDRESSES_PERMANENT_COUNTRYID;
-      permanentAddress.AddressLine1  = patron.ADDRESSES_PERMANENT_ADDRESSLINE1;
-      permanentAddress.AddressLine2  = patron.ADDRESSES_PERMANENT_ADDRESSLINE2;
-      permanentAddress.City  = patron.ADDRESSES_PERMANENT_CITY;
-      permanentAddress.Region  = patron.ADDRESSES_PERMANENT_REGION;
+
+      permanentAddress.AddressTypeId = patron.ADDRESSES_PERMANENT_ADDRESSTYPEID;
+      permanentAddress.CountryId = patron.ADDRESSES_PERMANENT_COUNTRYID;
+      permanentAddress.AddressLine1 = patron.ADDRESSES_PERMANENT_ADDRESSLINE1;
+      permanentAddress.AddressLine2 = patron.ADDRESSES_PERMANENT_ADDRESSLINE2;
+      permanentAddress.City = patron.ADDRESSES_PERMANENT_CITY;
+      permanentAddress.Region = patron.ADDRESSES_PERMANENT_REGION;
       /* TODO: normalize/format postal code */
-      permanentAddress.PostalCode  = patron.ADDRESSES_PERMANENT_POSTALCODE;
-  
+      permanentAddress.PostalCode = normalizePostalCode(patron.ADDRESSES_PERMANENT_POSTALCODE);
+
       personal.addresses.push(permanentAddress);
     }
-  
+
     if (patron.ADDRESSES_TEMPORARY_ADDRESSLINE1) {
       var temporaryAddress = {};
-  
+
       temporaryAddress.AddressTypeId = patron.ADDRESSES_TEMPORARY_ADDRESSTYPEID;
       temporaryAddress.AddressLine1 = patron.ADDRESSES_TEMPORARY_ADDRESSLINE1;
       temporaryAddress.AddressLine2 = patron.ADDRESSES_TEMPORARY_ADDRESSLINE2;
       temporaryAddress.City = patron.ADDRESSES_TEMPORARY_CITY;
       temporaryAddress.Region = patron.ADDRESSES_TEMPORARY_REGION;
       /* TODO: normalize/format postal code */
-      temporaryAddress.PostalCode = patron.ADDRESSES_TEMPORARY_POSTALCODE;
-  
+      temporaryAddress.PostalCode = normalizePostalCode(patron.ADDRESSES_TEMPORARY_POSTALCODE);
+
       personal.addresses.push(temporaryAddress);
     }
-  
+
     if (patron.DEPARTMENTS_0) {
       /* userImport.departments.push(patron.DEPARTMENTS_0); */
     }
-  
+
     userImport.personal = personal;
-  
+
     users.push(userImport);
   }
   return users;
