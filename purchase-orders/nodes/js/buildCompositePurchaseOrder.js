d2713e33fb95a88394f15b71e55daf8962e867f7
Kevin Day <kday@library.tamu.edu>
2021-06-30 11:16:52 -0500

    Populate Order Line.

--- purchase-orders/nodes/js/buildCompositePurchaseOrder.js
+++ purchase-orders/nodes/js/buildCompositePurchaseOrder.js
@@ -7,17 +7,77 @@ var poNumber = JSON.parse(poNumberResponse).poNumber;
 
 var vendorId = JSON.parse(vendorLookupResponse).organizations[0].id;
 
+var marcOrderDataObj = JSON.parse(marcOrderData);
+
+var locations = JSON.parse(locationsResponse).locations;
+
+var materialTypes = JSON.parse(materialTypesResponse).mtypes;
+
+var electronic = marcOrderDataObj.electronicIndicator && marcOrderDataObj.electronicIndicator.toLowerCase() === "electronic";
+
+var orderLine = {
+  id: orderLineId,
+  cost: {},
+  locations: []
+};
+
+var compositePoLines = [
+  orderLine
+];
+
 var compositePurchaseOrder = {
   id: orderId,
-  orderType: 'One-Time',
-  reEncumber: false,
   approved: true,
-  workflowStatus: 'Open',
+  compositePoLines: compositePoLines,
+  orderType: 'One-Time',
   poNumber: poNumber,
+  reEncumber: false,
   vendor: vendorId,
-  compositePoLines: [{
-    id: orderLineId
-  }]
+  workflowStatus: 'Open'
 };
+
+var findLocationByName = function (locationName) {
+  for (var i = 0; i < locations.length; ++i) {
+    if (locationName === locations[i].name) return locations[i];
+  }
+};
+
+var findMaterialTypeByName = function (materialTypeName) {
+  for (var i = 0; i < materialTypes.length; ++i) {
+    if (materialTypeName === materialTypes[i].name) return materialTypes[i];
+  }
+};
+
+if (electronic) {
+  orderLine.orderFormat = 'Electronic Resource';
+
+  orderLine.eResource = {
+    activated: false,
+    createInventory: 'Instance, Holding',
+    trial: false,
+    accessProvider: vendorId
+  };
+
+  orderLine.cost.quantityElectronic = 1;
+  orderLine.cost.listUnitPriceElectronic = marcOrderDataObj.price;
+
+  orderLine.locations.push({
+    quantityElectronic: 1,
+    locationId: findLocationByName(permELocation)
+  });
+} else {
+  orderLine.physical = {
+    createInventory: 'Instance, Holding, Item',
+    materialType: findMaterialTypeByName(permLocation)
+  };
+
+  orderLine.cost.quantityPhysical = 1;
+  orderLine.cost.listUnitPrice = marcOrderDataObj.price;
+
+  orderLine.locations.push({
+    quantityPhysical: 1,
+    locationId: findLocationByName(permLocation)
+  });
+}
 
 execution.setVariableLocal('compositePurchaseOrder', S(JSON.stringify(compositePurchaseOrder)));
---
+++ purchase-orders/nodes/js/logger.js
@@ -0,0 +1,25 @@
+print('\nindex = ' + loopCounter);
+/* print('\nrecord = ' + records[loopCounter] + '\n'); */
+
+print('\npermELocation = ' + permELocation + '\n');
+print('\npermLocation = ' + permLocation + '\n');
+print('\nfiscalYearCode = ' + fiscalYearCode + '\n');
+print('\nloanType = ' + loanType + '\n');
+print('\ntextForElectronicResources = ' + textForElectronicResources + '\n');
+print('\nnoteType = ' + noteType + '\n');
+print('\nmaterialType = ' + materialType + '\n');
+
+print('\npoNumberResponse = ' + poNumberResponse + '\n');
+print('\nvendorLookupResponse = ' + vendorLookupResponse + '\n');
+print('\nfundLookupResponse = ' + fundLookupResponse + '\n');
+
+print('\nmarcOrderData = ' + marcOrderData + '\n');
+
+print('\ncompositePurchaseOrder = ' + compositePurchaseOrder + '\n');
+
+print('\ninstanceTypesResponse = ' + instanceTypesResponse + '\n');
+print('\nmaterialTypesResponse = ' + materialTypesResponse + '\n');
+/*print('\nlocationsResponse = ' + locationsResponse + '\n');*/
+print('\nitemNoteTypesResponse = ' + itemNoteTypesResponse + '\n');
+print('\ninstanceNoteTypesResponse = ' + instanceNoteTypesResponse + '\n');
+print('\nholdingsNoteTypesResponse = ' + holdingsNoteTypesResponse + '\n');
