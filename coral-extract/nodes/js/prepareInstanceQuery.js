e41b4562c12c44294dfb913671505fa50247dffc
Kevin Day <kday@library.tamu.edu>
2021-07-26 10:23:40 -0500

    Issue 103: Use combined normalizeCqlUrlArgument() function.

--- coral-extract/nodes/js/prepareInstanceQuery.js
+++ coral-extract/nodes/js/prepareInstanceQuery.js
@@ -1,6 +1,5 @@
 var FormatUtility = Java.type('org.folio.rest.utility.FormatUtility');
 var item = JSON.parse(coralItem);
-var cqlTitle = FormatUtility.normalizeCql(item.title);
-var instanceQuery = 'title==' + FormatUtility.normalizeUrlArgument(cqlTitle);
+var cqlTitle = FormatUtility.normalizeCqlUrlArgument(item.title);
 
-execution.setVariableLocal('instanceQuery', instanceQuery);
+execution.setVariableLocal('instanceQuery', 'title==' + cqlTitle);
