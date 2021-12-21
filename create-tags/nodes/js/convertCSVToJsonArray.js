d80eaac4ef832d780dd39a877d617c520901d47d
Jeremy Huff <jeremythuff@gmail.com>
2021-12-21 15:10:05 -0600

    Finished parsing csv

--- create-tags/nodes/js/convertCSVToJsonArray.js
+++ create-tags/nodes/js/convertCSVToJsonArray.js
@@ -1,21 +1,8 @@
-var tags = [];
-
+var MappingUtility = Java.type("org.folio.rest.utility.MappingUtility");
 var tagsJSON = MappingUtility.mapCsvToJson(tagsCSV);
-var tagsObj = JSON.parse(tagsJSON);
 
 if (logLevel === "DEBUG") {
   print('\ntags = ' + tagsJSON + '\n');
 }
 
-// for(var i=1;i<arrData.length;i++) {
-//     var tagLabel = arrData[i][0];
-//   tags.push({
-//     label: tagLabel
-//   });
-// }
-
-if (logLevel === "DEBUG") {
-    print('\ntags = ' + JSON.stringify(tags) + '\n');
-}
-
-execution.setVariable('tags', S(JSON.stringify(tags)));
+execution.setVariable('tags', S(tagsJSON));
