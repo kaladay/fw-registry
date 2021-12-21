fd68513d299b973d4222cb4f4e526d29ffa6bc11
Jeremy Huff <jeremythuff@gmail.com>
2021-12-21 12:59:34 -0600

    WIP using csv mapping util

--- create-tags/nodes/js/convertCSVToJsonArray.js
+++ create-tags/nodes/js/convertCSVToJsonArray.js
@@ -1,57 +1,18 @@
 var tags = [];
 
-strDelimiter = ",";
+var tagsJSON = MappingUtility.mapCsvToJson(tagsCSV);
+var tagsObj = JSON.parse(tagsJSON);
 
-var objPattern = new RegExp(
-  (
-    "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
-
-    "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
-
-    "([^\"\\" + strDelimiter + "\\r\\n]*))"
-  ),
-  "gi"
-);
-
-var arrData = [[]];
-
-var arrMatches = null;
-
-while (arrMatches = objPattern.exec( tagsCSV )){
-
-  var strMatchedDelimiter = arrMatches[ 1 ];
-
-  if (
-    strMatchedDelimiter.length &&
-    (strMatchedDelimiter != strDelimiter)
-  ){
-
-    arrData.push( [] );
-
-  }
-
-  if (arrMatches[ 2 ]){
-
-    var strMatchedValue = arrMatches[ 2 ].replace(
-      new RegExp( "\"\"", "g" ),
-      "\""
-    );
-
-  } else {
-
-    var strMatchedValue = arrMatches[ 3 ];
-
-  }
-
-  arrData[ arrData.length - 1 ].push( strMatchedValue );
+if (logLevel === "DEBUG") {
+  print('\ntags = ' + tagsJSON + '\n');
 }
 
-for(var i=1;i<arrData.length;i++) {
-    var tagLabel = arrData[i][0];
-  tags.push({
-    label: tagLabel
-  });
-}
+// for(var i=1;i<arrData.length;i++) {
+//     var tagLabel = arrData[i][0];
+//   tags.push({
+//     label: tagLabel
+//   });
+// }
 
 if (logLevel === "DEBUG") {
     print('\ntags = ' + JSON.stringify(tags) + '\n');
