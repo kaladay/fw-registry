089e93505173d21c7aef82aefdd640f4d95e02f2
Jeremy Huff <jeremythuff@gmail.com>
2022-06-01 14:37:57 -0500

    WIP

--- shelflist-items/nodes/js/buildShelflistQuery.js
+++ shelflist-items/nodes/js/buildShelflistQuery.js
@@ -15,11 +15,11 @@ if (logLevel === 'DEBUG') {
   print('updatedDateEnd = ' + updatedDateEnd + '\n');
 }
 
-var from = 'folio_reporting.items_ext'
-            + '\n\tinner join public.inventory_items ii on item_ext.item_id = ii.id'
-            + '\n\tinner join folio_reporting.items_holdings_instances ihi on item_ext.item_id = ihi.item_id'
-            + '\n\tinner join public.inventory_holdings ih on ihi.holdings_record_id = ih.id'
-            + '\n\tleft join mis.item_history history on item_ext.item_id = history.item_id';
+var from = 'folio_reporting.item_ext ie'
+            + '\n\tinner join folio_reporting.holdings_ext he on ie.holdings_record_id =  he.holdings_id'
+            + '\n\tleft join mis.item_history history on ie.item_id = history.item_id'
+            + '\n\tleft join folio_reporting.instance_ext inste on inste.instance_id = he.instance_id'
+            + '\n\tleft join public.inventory_items ii on ii.id = ie.item_id';
 
 var where = 'TRUE';
 
@@ -35,7 +35,7 @@ var normalizeArray = function (array) {
 if (libraryNameArray) {
   normalizeArray(libraryNameArray);
   if (libraryNameArray.length > 0) {
-    from += '\n\tLEFT JOIN public.inventory_locations publoc ON items_ext.permanent_location_id = publoc.id'
+    from += '\n\tLEFT JOIN public.inventory_locations publoc ON ie.permanent_location_id = publoc.id'
           + '\n\tLEFT JOIN public.inventory_libraries publib ON publoc.library_id = publib.id';
 
     where += '\n\tAND publib.name IN (\'' + libraryNameArray.join('\',\'') + '\')';
@@ -48,7 +48,7 @@ if (locationDiscoveryDisplayNameArray) {
   normalizeArray(locationDiscoveryDisplayNameArray);
 
   if (locationDiscoveryDisplayNameArray.length > 0) {
-    from += '\n\tLEFT JOIN public.inventory_locations publoc ON items_ext.permanent_location_name = publoc.name';
+    from += '\n\tLEFT JOIN public.inventory_locations publoc ON ie.permanent_location_name = publoc.name';
 
     where += '\n\tAND publoc.discovery_display_name IN (\'' + locationDiscoveryDisplayNameArray.join('\',\'') + '\')';
   }
@@ -60,7 +60,7 @@ if (locationNameArray) {
   normalizeArray(locationNameArray);
 
   if (locationNameArray.length > 0) {
-    where += '\n\tAND items_ext.permanent_location_name IN (\'' + locationNameArray.join('\',\'') + '\')';
+    where += '\n\tAND ie.permanent_location_name IN (\'' + locationNameArray.join('\',\'') + '\')';
   }
 }
 
@@ -70,7 +70,7 @@ if (loanTypeArray) {
   normalizeArray(loanTypeArray);
 
   if (loanTypeArray.length > 0) {
-    where += '\n\tAND items_ext.permanent_loan_type_name IN (\'' + loanTypeArray.join('\',\'') + '\')';
+    where += '\n\tAND ie.permanent_loan_type_name IN (\'' + loanTypeArray.join('\',\'') + '\')';
   }
 }
 
@@ -80,7 +80,7 @@ if (materialTypeArray) {
   normalizeArray(materialTypeArray);
 
   if (materialTypeArray.length > 0) {
-    where += '\n\tAND items_ext.material_type_name IN (\'' + materialTypeArray.join('\',\'') + '\')';
+    where += '\n\tAND ie.material_type_name IN (\'' + materialTypeArray.join('\',\'') + '\')';
   }
 }
 
@@ -90,52 +90,52 @@ if (itemStatusArray) {
   normalizeArray(itemStatusArray);
 
   if (itemStatusArray.length > 0) {
-    where += '\n\tAND items_ext.tatus_name IN (\'' + itemStatusArray.join('\',\'') + '\')';
+    where += '\n\tAND ie.tatus_name IN (\'' + itemStatusArray.join('\',\'') + '\')';
   }
 }
 
 if (createdDateStart != '') {
-  where += '\n\tAND cast(to_timestamp(instance_ext.record_created_date::text,\'YYYY-MM-DD\') AT TIME ZONE \'America/Chicago\' AS DATE) >= to_date(\'' + createdDateStart + '\', \'YYYY-MM-DD\')';
+  where += '\n\tAND cast(to_timestamp(inste.record_created_date::text,\'YYYY-MM-DD\') AT TIME ZONE \'America/Chicago\' AS DATE) >= to_date(\'' + createdDateStart + '\', \'YYYY-MM-DD\')';
 }
 
 if (createdDateEnd != '') {
-  where += '\n\tAND cast(to_timestamp(instance_ext.record_created_date::text,\'YYYY-MM-DD\') AT TIME ZONE \'America/Chicago\' AS DATE) <= to_date(\'' + createdDateEnd + '\', \'YYYY-MM-DD\')';
+  where += '\n\tAND cast(to_timestamp(inste.record_created_date::text,\'YYYY-MM-DD\') AT TIME ZONE \'America/Chicago\' AS DATE) <= to_date(\'' + createdDateEnd + '\', \'YYYY-MM-DD\')';
 }
 
 if (updatedDateStart != '') {
-  where += '\n\tAND cast(to_timestamp(instance_ext.updated_date::text,\'YYYY-MM-DD\') AT TIME ZONE \'America/Chicago\' AS DATE) >= to_date(\'' + updatedDateStart + '\', \'YYYY-MM-DD\')';
+  where += '\n\tAND cast(to_timestamp(inste.updated_date::text,\'YYYY-MM-DD\') AT TIME ZONE \'America/Chicago\' AS DATE) >= to_date(\'' + updatedDateStart + '\', \'YYYY-MM-DD\')';
 }
 
 if (updatedDateEnd != '') {
-  where += '\n\tAND cast(to_timestamp(instance_ext.updated_date::text,\'YYYY-MM-DD\') AT TIME ZONE \'America/Chicago\' AS DATE) <= to_date(\'' + updatedDateEnd + '\', \'YYYY-MM-DD\')';
+  where += '\n\tAND cast(to_timestamp(inste.updated_date::text,\'YYYY-MM-DD\') AT TIME ZONE \'America/Chicago\' AS DATE) <= to_date(\'' + updatedDateEnd + '\', \'YYYY-MM-DD\')';
 }
 
 var shelflistQuery = '\n'
        + '\nSELECT DISTINCT'
-       + '\n\tinstance_ext.item_hrid AS item_hrid,'
-       + '\n\tinstance_ext.barcode AS barcode,'
-       + '\n\titem_ext.effective_location_name AS location,'
-       + '\n\titem_ext.effective_call_number,'
-       + '\n\titem_ext.enumeration,'
-       + '\n\titem_ext.chronology,'
-       + '\n\tsubstring(ihi.title,1,60) AS title,'
+       + '\n\tie.item_hrid AS item_hrid,'
+       + '\n\tie.barcode AS barcode,'
+       + '\n\tie.effective_location_name AS location,'
+       + '\n\tie.effective_call_number,'
+       + '\n\tie.enumeration,'
+       + '\n\tie.chronology,'
+       + '\n\tsubstring(inste.title,1,60) AS title,'
        + '\n\thistory.hist_charges AS hist_charges,'
        + '\n\thistory.hist_browses AS hist_browses,'
        + '\n\thistory.last_transaction AS last_transaction,'
-       + '\n\titem_ext.status_name,'
+       + '\n\ie.status_name,'
        + '\n\tCASE'
-       + '\n\t\tWHEN item_ext.temporary_loan_type_name is null THEN item_ext.permanent_loan_type_name'
-       + '\n\t\tELSE item_ext.temporary_loan_type_name'
-       + '\n\tEND as loan_type'
-       + '\n\titem_ext.material_type_name,'
-       + '\n\titem_ext.discovery_suppress,'
-       + '\n\tcast(to_timestamp(item_ext.created_date::text,\'YYYY-MM-DD\') at time zone \'America/Chicago\' as date) as create_date,'
-       + '\n\tcast(to_timestamp(item_ext.updated_date::text,\'YYYY-MM-DD\') at time zone \'America/Chicago\' as date) as update_date,'
-       + '\n\tii.effective_shelving_order COLLATE "C" AS shelving_order,'
-       + '\n\tih.hrid AS holdings_hrid'
+       + '\n\t\tWHEN ie.temporary_loan_type_name is null THEN ie.permanent_loan_type_name'
+       + '\n\t\tELSE ie.temporary_loan_type_name'
+       + '\n\tEND as loan_type,'
+       + '\n\ie.material_type_name,'
+       + '\n\ie.discovery_suppress,'
+       + '\n\tcast(to_timestamp(ie.created_date::text,\'YYYY-MM-DD\') at time zone \'America/Chicago\' as date) as create_date,'
+       + '\n\tcast(to_timestamp(ie.updated_date::text,\'YYYY-MM-DD\') at time zone \'America/Chicago\' as date) as update_date,'
+       + '\n\tii.effective_shelving_order COLLATE '+decodeURI("%22")+'C'+decodeURI("%22")+' AS shelving_order,'
+       + '\n\the.holdings_hrid AS holdings_hrid'
        + '\nFROM ' + from
        + '\nWHERE ' + where
-       + '\nORDER BY order by location, shelving_order, enumeration, chronology, holdings_hrid\n';
+       + '\nORDER BY location, shelving_order, enumeration, chronology, holdings_hrid\n';
 
 if (logLevel === 'DEBUG') {
   print('\nshelflistQuery = ' + shelflistQuery);
