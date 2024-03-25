57759cd7698e3455fc2c3a28c556a13dd5336fcc
William Welling <wwelling@tamu.edu>
2024-03-25 15:57:35 -0500

    remove-books-from-nbs: Remove skipped barcodes from email attachment

--- remove-books-from-nbs/nodes/js/buildEmail.js
+++ remove-books-from-nbs/nodes/js/buildEmail.js
@@ -5,18 +5,6 @@ var emailSubject = 'Items removed from new bookshelf';
 var emailMarkup = '<p>A total of <strong>' + itemsRemoved.length + '</strong> items have been successfully processed. Attached is the CSV file of the barcodes of the items removed from the New Bookshelf. </p>\n';
 var emailText = '';
 
-
-emailMarkup += '<p>Here are the list of items skipped from the input barcodes.</p><p>';
-for (var i = 0; i < skippedItems.length; ++i) {
-  var item = skippedItems[i];
-  emailMarkup += '<p>Item: ' + item.title + '</p>';
-  emailMarkup += '<ul>';
-  emailMarkup += '<li>\tid: ' + item.id + '</li>';
-  emailMarkup += '<li>\tbarcode: ' + item.barcode + '</li>';
-  emailMarkup += '</ul>';
-}
-emailMarkup += '</p>';
-
 emailText = emailMarkup.replace(/<\/p>/ig, '\n')
   .replace(/<\/li>/ig, '\n')
   .replace(/<\/ul>/ig, '\n')
