abeb7c4ee766493b845f57426005a79ba7bfc116
Rajdeep Hundal <rhundal7@tamu.edu>
2024-06-14 13:40:47 -0500

    fixed source and destination paths

--- examples/example-compressfiletask-zip/nodes/rb/createTargetVar.rb
+++ examples/example-compressfiletask-zip/nodes/rb/createTargetVar.rb
@@ -1,4 +1,11 @@
 
+require 'json'
+
+query_wrapper = {
+  message: 'Hello, File From the Script Task!'
+}
+
+json_string  = query_wrapper.to_json
 execution.setVariableLocal('exampleFileData', json_string)
 
 
